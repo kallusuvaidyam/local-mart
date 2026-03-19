4c0256cecaca346007a2a6df6a6a280ee44f9033
kallu.sah <kallu.sah@suvaidyam.com>
2026-03-19 12:44:42 +0530

    added vercel analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
-import './assets/main.css'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
+import { inject } from "@vercel/analytics";
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
-app.mount('#app')
+inject();
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
